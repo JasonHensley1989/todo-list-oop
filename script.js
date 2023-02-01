4cdc4ba305d8bab8021c2ac911aede6635ee52f4
Jason Hensley <jasonhensley1989@gmail.com>
2023-01-31 21:50:07 -0600

    add event listener issue fixed problem being script needed to be placed at bottom of body

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("Javascript Active")
+let form = document.getElementById("form")
+let toDo = document.getElementById("toDo");
+let firstToDo = document.getElementById("firstToDo")
+
+firstToDo.addEventListener('click', () => {
+    console.log("event listener working")
+})
+
