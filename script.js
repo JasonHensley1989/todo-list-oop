44821db0388a17934450007f0084b2e9ffc9e49f
Jason Hensley <jasonhensley1989@gmail.com>
2023-02-01 11:02:58 -0600

    pause in creating static display

--- script.js
+++ script.js
@@ -11,8 +11,7 @@ let todoArr = [];
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     // this generates a random number to create a unique id for the list value
-    let id = Math.random() * 200000;
-    console.log(id)
+    let id = Math.ceil(Math.random() * 200000);
     // on button click the event listener will produce a new oject 
     // holding the values of a new id, and the actual text of the object
     const todo = new Todo(id, input.value);
@@ -28,4 +27,12 @@ class Todo {
         this.todo = todo;
     }
 }
+
+// this will allow the data to be displayed in the DOM taking in the todoArr
+// class UI {
+//      static displayData() {
+
+//      }
+// }
+
 
